ed0c1e29bc3edbc49eca29f6f0a4afc0d7f84417
Ken Peterson <kenpeterson3@gmail.com>
2017-09-08 00:21:28 -0400

    w2d3 exercises 70 perecent done

--- m1-w2d3-collections-part2-exercises/src/main/java/com/techelevator/Exercises.java
+++ m1-w2d3-collections-part2-exercises/src/main/java/com/techelevator/Exercises.java
@@ -49,16 +49,12 @@ public class Exercises {
 		animalGroupName.put("Deer", "Herd");
 		animalGroupName.put("Dog", "Pack");
 		animalGroupName.put("Crocodile", "Float");
-		//System.out.println(animalGroupName);
-		//for (String  : )
-		
+	
 		if (!animalGroupName.containsKey(animalName)){
 			return "unknown";
-		}
-		
+		}		
 		return animalGroupName.get(animalName);
 	}
-
 	/*
 	 * Given an String item number (a.k.a. SKU), return the discount percentage if the item is on sale.
 	 * If the item is not on sale, return 0.00.
@@ -94,11 +90,9 @@ public class Exercises {
 		
 		if (isItOnSale.containsKey(itemNumber.toUpperCase())){
 			return isItOnSale.get(itemNumber.toUpperCase());
-		}
-		
+		}		
 		return 0.00;
-	}
-	
+	}	
 	/*
 	 * Modify and return the given map as follows: if "Peter" has more than 0 money, transfer half of it to "Paul",
 	 * but only if Paul has less than $10s.
@@ -124,16 +118,13 @@ public class Exercises {
 				paulMoney = peterMoney + paulMoney;
 			}else{ 
 				return peterPaul;
-			}
-			
-		}
-		
+			}			
+		}		
 		peterPaul.put("Peter", peterMoney);
 		peterPaul.put("Paul", paulMoney);
 		
 		return peterPaul;
-	}
-	
+	}	
     /*
 	 * Modify and return the given map as follows: if "Peter" has $50 or more, AND "Paul" has $100 or more,
 	 * then create a new "PeterPaulPartnership" worth a combined contribution of a quarter of each partner's
@@ -147,8 +138,7 @@ public class Exercises {
 		
 		int peterMoney = peterPaul.get("Peter");
 		int paulMoney = peterPaul.get("Paul");
-	
-		
+			
 		if ( peterMoney >= 5000 && paulMoney >= 10000) {
 			int peterPaulPartnershipPeter = peterMoney / 4;
 			peterMoney = peterMoney - peterPaulPartnershipPeter;
@@ -159,12 +149,10 @@ public class Exercises {
 			peterPaul.put("Paul", paulMoney);
 			peterPaul.put("PeterPaulPartnership", total);
 			return peterPaul;
-		}else{
-		
+		}else{		
 		return peterPaul;
 		}
-	}
-	
+	}	
 	/*
 	 * Given an array of non-empty strings, return a Map<String, String> where for every different string in the array, 
 	 * there is a key of its first character with the value of its last character.
@@ -174,6 +162,7 @@ public class Exercises {
 	 * beginningAndEnding(["muddy", "good", "moat", "good", "night"]) → {"g": "d", "m": "t", "n": "t"}
 	 */
 	public Map<String, String> beginningAndEnding(String[] words) {
+		
 		
 		
 		return null;
@@ -201,10 +190,8 @@ public class Exercises {
 				counts.put(word,  currentCount) ;
 			} else {
 				counts.put(word, 1);
-			}
-			
-		}
-			
+			}			
+		}			
 		return counts;
 	}
 	
@@ -221,11 +208,19 @@ public class Exercises {
 	 */
 	public Map<Integer, Integer> integerCount(int[] ints) {
 	
+		Map<Integer, Integer> counts = new HashMap<>();
 		
-		
-		return null;
-	}
-	
+		for (int integerCheck : ints) {
+			if(counts.containsKey(integerCheck)) {
+				int currentCount = counts.get(integerCheck);
+				currentCount++;
+				counts.put(integerCheck, currentCount) ;
+			} else {
+				counts.put(integerCheck, 1);
+			}	
+		}					
+		return counts;
+	}	
 	/*
 	 * Given an array of strings, return a Map<String, Boolean> where each different string is a key and value
 	 * is true only if that string appears 2 or more times in the array.
@@ -236,8 +231,28 @@ public class Exercises {
 	 * 
 	 */
 	public Map<String, Boolean> wordMultiple(String[] words) {
-		return null;
-	}
+		Map<String, Boolean> wordMultiple = new HashMap<>();//return this map after feeding in Integer count results
+		Map<String, Integer> trueTwoOrMore = new HashMap<>();//use this map to run count
+		
+		for  (String multipleTimes : words ) {
+						
+			if(trueTwoOrMore.containsKey(multipleTimes)) {
+				int currentCount = trueTwoOrMore.get(multipleTimes);
+				currentCount++;
+				if (currentCount > 1){
+				wordMultiple.put(multipleTimes, true);
+				}else{ wordMultiple.put(multipleTimes, false);					 
+				}
+			} else { 
+				trueTwoOrMore.put(multipleTimes, 1);
+				wordMultiple.put(multipleTimes, false);					 
+			}
+			} 
+		return wordMultiple;
+		}
+		
+		
+	
 	
 	/*
 	 * Given two maps, Map<String, Integer>, merge the two into a new map, Map<String, Integer> where keys in Map2, 
